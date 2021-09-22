1cec6b9c3c11cca044a8befea66fea79cc1c64e1
samuelmiller36 <samuelmiller224@gmail.com>
2021-09-22 07:42:46 -0400

    R 0.1
    Testing a possible solution for docker build overhead.

---
+++ init.sh
@@ -0,0 +1,30 @@
+npm install gulp && npm install gulp-cli
+npm install apache-server-configs
+npm install connect
+npm install connect-livereload
+npm install del
+npm install gulp-autoprefixer
+npm install gulp-cache
+npm install gulp-csso
+npm install gulp-filter
+npm install gulp-flatten
+npm install gulp-if
+npm install gulp-imagemin
+npm install gulp-jshint
+npm install gulp-livereload
+npm install gulp-load-plugins
+npm install gulp-minify-html
+npm install gulp-plumber
+npm install gulp-sass
+npm install gulp-size
+npm install gulp-uglify
+npm install gulp-useref
+npm install jshint
+npm install jshint-stylish
+npm install main-bower-files
+npm install opn
+npm install serve-index
+npm install serve-static
+
+git clone https://github.com/glua/load-seed.git
+mv load-seed-master /.
